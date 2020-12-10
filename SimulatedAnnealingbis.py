bf4fe1052ad0f69a50addfe5665441714f10b0cb
Amine Chaouachi <spaghet@pop-os.localdomain>
2020-12-10 15:55:33 +0100

    Added second method

---
+++ SimulatedAnnealingbis.py
@@ -0,0 +1,137 @@
+import typing
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import scipy.spatial.distance as dist
+import copy
+
+
+
+def distance(x: np.ndarray, y: np.ndarray) -> float:
+    return np.linalg.norm(x - y)
+
+
+def mean(x: np.ndarray, y: np.ndarray) -> np.ndarray:
+    # print(x)
+    # print(y)
+    return np.array([(x[0] + y[0]) / 2, (x[1] + y[1]) / 2])
+
+
+class Solution(object):
+    def __init__(self, lmbd: float, dataset) -> None:
+        self._lmbd = lmbd
+        self._dataset = dataset
+        self._c_i: typing.Optional[int] = np.random.randint(dataset.N)
+        self._c_j: typing.Optional[int] = np.random.randint(dataset.N)
+        self._center: typing.Optional[np.ndarray] = np.array([0, 0])
+        self._radius: float = 0
+        self._assignments = np.zeros(self._dataset.N) == 1
+        self._objective_v: float = 0
+        self.update()
+
+    def update(self) -> None:
+
+        self._radius = self.index_distance(self._c_i, self._c_j) / 2
+        self._center = self.index_mean(self._c_i, self._c_j)
+
+        for j in range(self._dataset.N):
+            dist_ = distance(self._dataset.x[j], self._center)
+
+            if self._assignments[j]:
+                if dist_ > self._radius:
+                    self._assignments[j] = not self._assignments[j]
+                    self._objective_v -= self._dataset.v[j]
+            else:
+                if dist_ <= self._radius or j == self._c_i or j == self._c_j:
+                    self._assignments[j] = not self._assignments[j]
+                    self._objective_v += self._dataset.v[j]
+
+    def move(self, move: int) -> None:
+
+        point_to_replace = np.random.choice((self._c_i, self._c_j))
+
+        if point_to_replace == self._c_i:
+            self._c_i = move
+        else:
+            self._c_j = move
+
+        if self._c_i == self._c_j:
+            self._radius = 0
+            self._center = self._dataset.x[self._c_i]
+            self._assignments = np.zeros(self._dataset.N) == 1
+            self._assignments[self._c_i] = True
+            self._objective_v = self._dataset.v[self._c_i]
+
+        else:
+            self.update()
+
+
+
+    def get_objective(self) -> float:
+        return self._objective_v - self._lmbd * self._dataset.N * np.pi * np.power(self._radius, 2)
+
+    def plot(self) -> None:
+        a = sns.relplot(self._dataset.x[:, 0], self._dataset.x[:, 1], hue=self._assignments, size=self._dataset.v)
+        if self._center is not None:
+            a.ax.add_artist(plt.Circle(tuple(self._center), self._radius, color='black', fill=False))
+            a.ax.add_artist(
+                plt.Circle(self._dataset.x[self._c_i], 2 * self._radius, color='grey', linestyle='--', fill=False))
+            a.ax.add_artist(
+                plt.Circle(self._dataset.x[self._c_j], 2 * self._radius, color='grey', linestyle='--', fill=False))
+        plt.title("s = " + str(np.round(self._objective_v, 2)) + " ; r = " + str(np.round(self._radius, 2)))
+        plt.show()
+
+    def index_distance(self, i: int, j: int) -> float:
+        return distance(self._dataset.x[i], self._dataset.x[j])
+
+    def index_assigned_distances(self, assigned):
+        assigned_points = self._dataset.x[assigned]
+        return dist.pdist(assigned_points)
+
+    def index_mean(self, i: int, j: int) -> np.ndarray:
+        return mean(self._dataset.x[i], self._dataset.x[j])
+
+
+class SimulatedAnnealing(object):
+    def __init__(self, lmbd: float, dataset, p: float):
+        self._lmbd = lmbd
+        self._dataset = dataset
+        self._p = p
+        self.S = self._initial_solution()
+        self.S.plot()
+        self.objectives = [self.S.get_objective()]
+        self._beta = self._heat_up()
+        print(self._beta)
+
+    def _initial_solution(self) -> Solution:
+        initial_solution: Solution = Solution(self._lmbd, self._dataset)
+        # for i in np.random.randint(self._dataset.N, size=int(self._dataset.N / 2)):
+        for i in [np.random.randint(self._dataset.N)]:
+            initial_solution.move(i)
+        return initial_solution
+
+    def _heat_up(self) -> float:
+        old_obj = self.S.get_objective()
+        sum_obj = 0
+        count = 0
+        for i in range(self._dataset.N):
+            # TODO
+            s_prime = copy.deepcopy(self.S)
+            s_prime.move(i)
+            new_obj = s_prime.get_objective()
+            if new_obj < old_obj:
+                sum_obj += new_obj
+                count += 1
+        return np.log(self._p) / (sum_obj / count - self.S.get_objective())
+
+    def cool_down(self, iters: int) -> None:  # TODO
+        for i in tqdm(range(iters)):
+            old_objective = self.S.get_objective()
+            move = np.random.randint(self._dataset.N)
+            s_prime = copy.deepcopy(self.S)
+            s_prime.move(move)
+            if np.random.random() <= np.exp(self._beta * (s_prime.get_objective() - old_objective)):
+                self.S = s_prime
+            self.objectives.append(self.S.get_objective())
+        self.S.plot()
